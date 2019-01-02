95501a9828d328c130b671f468c9b57dda8e51b2
jlpospisil <josh.pospisil@gmail.com>
2019-01-02 13:25:15 -0600

    Added async component examples

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,31 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <About msg="About Us"/>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import Loading from '@/components/ui/Loading.vue';
+
+const AsyncAbout = () => ({
+  // The component to load (should be a Promise)
+  component: import(/* webpackChunkName: "example-component" */ '@/components/ExampleComponent.vue'),
+  // A component to use while the async component is loading
+  loading: Loading,
+  // A component to use if the load fails
+  // error: ErrorComponent,
+  // Delay before showing the loading component. Default: 200ms.
+  // delay: 200,
+  // The error component will be displayed if a timeout is provided and exceeded. Default: Infinity.
+  // timeout: 4000,
+});
+
+export default {
+  name: 'about',
+  components: {
+    About: AsyncAbout,
+  },
+};
+</script>
--- src/views/Error.vue
+++ src/views/Error.vue
@@ -1,32 +1,42 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <Home msg="Home Page"/>
+    <Error msg="This will never show"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Loading from '@/components/ui/Loading.vue';
+import Error from '@/components/ExampleComponent.vue';
+
+const error = {
+  template: `
+    <div style="font-style: italic;">
+      <div>This is an example of a timeout loading a component asynchronously.</div>
+      <div>The loading component is displayed until the timeout is reached, at which point the error component is displayed.</div>
+    </div>
+  `,
+};
 
 // component would typically use import(), but timeout allows for simulating slow loading component
-const AsyncHome = () => ({
+const AsyncError = () => ({
   // The component to load (should be a Promise)
-  component: import(/* webpackChunkName: "example-component" */ '@/components/ExampleComponent.vue'),
+  component: new Promise(resolve => setTimeout(() => resolve(Error), 5000)),
   // A component to use while the async component is loading
   loading: Loading,
   // A component to use if the load fails
-  // error: ErrorComponent,
+  error,
   // Delay before showing the loading component. Default: 200ms.
-  // delay: 200,
+  delay: 200,
   // The error component will be displayed if a timeout is provided and exceeded. Default: Infinity.
-  // timeout: 4000,
+  timeout: 3000,
 });
 
 export default {
   name: 'home',
   components: {
-    Home: AsyncHome,
+    Error: AsyncError,
   },
 };
 </script>
